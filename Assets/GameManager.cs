7fee76bdf041bd6351ea73f7d2cfce9fc7196acd
kindezen <kindezen@naver.com>
2020-11-24 23:00:04 +0900

    First Upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+
+    public GameObject startPage;
+    public GameObject[] stages;
+    public GameObject endPage;
+
+    private int maxScore
+    {
+        get
+        {
+            return stages.Length;
+        }
+    }
+    private int totScore;
+
+    private int nowStage;
+
+    void InitGame()
+    {
+        totScore = 0;
+    }
+
+    public void StartGame()
+    {
+        InitGame();
+
+        startPage.SetActive(false);
+        SetStage(0);
+    }
+
+    public void GoToNextStage(bool isSuccess)
+    {
+        if (isSuccess)
+            totScore++;
+
+        SetStage(nowStage + 1);
+    }
+
+    void SetStage(int _stage)
+    {
+        nowStage = _stage;
+
+        foreach(var item in stages)
+            item.SetActive(false);
+
+        if (_stage >= maxScore)
+        {
+            SetEndPage();
+            return;
+        }
+
+        stages[nowStage].SetActive(true);
+        stages[nowStage].GetComponent<IStage>().InitStage();
+    }
+
+    void SetEndPage()
+    {
+        endPage.SetActive(true);
+
+        endPage.transform.Find("Text_Score").GetComponent<Text>().text = totScore.ToString() + "/" + maxScore.ToString();
+    }
+
+}
---
+++ Assets/Stage1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface IStage
+{
+    void InitStage();
+}
+
+public class Stage1 : MonoBehaviour, IStage
+{
+
+    public Button baechulBtn;
+    public Button changeBtn;
+    public int answerCount;
+    
+    private int currentCount;
+
+    public void InitStage()
+    {
+        currentCount = 0;
+
+        baechulBtn.onClick.AddListener(() =>
+        {
+            if (currentCount == answerCount)
+                StageClear();
+            else
+                StageFail();
+        });
+
+        changeBtn.onClick.AddListener(() =>
+        {
+            ChangeState();
+        });
+    }
+
+    public void ChangeState()
+    {
+        currentCount++;
+
+        if(currentCount > answerCount)
+        {
+            StageFail();
+        }
+
+        //TODO: 이미지 변경
+    }
+
+    void StageFail()
+    {
+        GameManager.Instance.GoToNextStage(false);
+    }
+
+    void StageClear()
+    {
+        GameManager.Instance.GoToNextStage(true);
+    }
+
+}
